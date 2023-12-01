cd26776ee58bbcf97706c5e6d11812fed0632c82
Mirko <@miroslav.magerov.7e5@itb.cat>
2023-12-01 12:28:00 +0100

    Casi acabado el bucle del teclado numerico

--- prueba/codigo.js
+++ prueba/codigo.js
@@ -1,15 +1,32 @@
 const divPrincipal = document.getElementById("contenedor");
+const body = document.getElementById("body");
 
 let counter = 1;
 
+let saltoLinea = document.createElement("br");
+let userNumberCounter;
+
+userNumberCounter = prompt("Escribe la cantidad de teclas que quieres: ");
+
 divPrincipal.className = "divPrincipal";
 
-for (let i = 0; i < 9; i++) {
-    let divTecla = document.createElement("div");
+for (let i = 0; i < userNumberCounter; i++) {
+    let divTecla = document.createElement("divTecla");
     divTecla.className = "divTecla";
     divTecla.textContent = counter;
     divPrincipal.appendChild(divTecla);
     counter++;
-};
+}
 
-console.log("5" == 5)
+if (userNumberCounter > 0) {
+    let j = 0;
+    counter = 1;
+    while (j < userNumberCounter) {
+        let divTecla = document.createElement("divTecla");
+        divTecla.className = "divTecla";
+        divTecla.textContent = counter;
+        divPrincipal.appendChild(divTecla);
+        counter++;
+        j++;
+    }
+}
